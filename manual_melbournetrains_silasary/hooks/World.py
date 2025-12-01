645c288328da817cb5f5b8f5bb7e36b753be839d
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-12-01 13:13:40 +1100

    Connect Deer Park

--- manual_melbournetrains_silasary/hooks/World.py
+++ manual_melbournetrains_silasary/hooks/World.py
@@ -38,7 +38,7 @@ def hook_get_filler_item_name(world: World, multiworld: MultiWorld, player: int)
 
 # Called before regions and locations are created. Not clear why you'd want this, but it's here. Victory location is included, but Victory event is not placed yet.
 def before_create_regions(world: World, multiworld: MultiWorld, player: int):
-    pass
+    world.options.start_hints.value.add("Bounty")
 
 # Called after regions and locations are created, in case you want to see or modify that information. Victory location is included.
 def after_create_regions(world: World, multiworld: MultiWorld, player: int):
